52ee106af9c695b7fbea550d6392f562824a2521
kqakqakqa <58761476+kqakqakqa@users.noreply.github.com>
2025-07-08 23:58:41 +0800

    update: 逻辑优化

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -6,142 +6,41 @@ export default {
   data: {
     showLeftSideSwipe: false,
     showTopSideSwipe:false,
-    leftSideSwipeTimeout:undefined,
-    topSideSwipeTimeout: undefined,
-    grid: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    leftSideSwipeTimeout:null,
+    topSideSwipeTimeout: null,
+    grid: [],
     score: 0,
     highScore: 0,
-    getCellBgColor(n) {
-      return {
-        "0":"#ccc0b4",
-        "2":"#eee4da",
-        "4":"#ede0c8",
-        "8":"#f2b179",
-        "16":"#f59563",
-        "32":"#f67c5f",
-        "64":"#f65e3b",
-        "128":"#edcf72",
-        "256":"#edcc61",
-        "512":"#edc850",
-        "1024":"#edc53f",
-        "2048":"#edc22e",
-        "4096":"#edbf1d",
-        "8192":"#edbc0c",
-        "16384":"#edb900",
-        "32768":"#edb600",
-        "65536":"#edb300",
-        "131072":"#edb000",
-      }[n];
+    refresh: true,
+    cellBgColor: {
+      0: "#ccc0b4",
+      2: "#eee4da",
+      4: "#ede0c8",
+      8: "#f2b179",
+      16: "#f59563",
+      32: "#f67c5f",
+      64: "#f65e3b",
+      128: "#edcf72",
+      256: "#edcc61",
+      512: "#edc850",
+      1024: "#edc53f",
+      2048: "#edc22e",
+      4096: "#edbf1d",
+      8192: "#edbc0c",
+      16384: "#edb900",
+      32768: "#edb600",
+      65536: "#edb300",
+      131072: "#edb000",
     },
-    getCellColor(n){
-      return (n>4)?"#f9f6f2":"#776e65";
-    },
-    getCellFontSize(n){
-      if(n<16) return "30px";
-      if(n<128) return "30px;"
-      if(n<1024) return "26px;"
-      if(n<16384) return "26px;"
-      return "22px;"
-    },
-    newGrid(){
-      let grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-      grid = this.addCellNumber(grid);
-      grid = this.addCellNumber(grid);
-      return grid;
-    },
-    addCellNumber(grid){
-      if(this.isGridFull(grid)) return grid;
-      let idx = Math.floor(Math.random() * 16);
-      while(1) {
-        for(let i in grid){
-          for(let j in grid[i]){
-            if(grid[i][j]===0) idx -= 1;
-            if(idx < 0) {
-              grid[i][j] = Math.random() < 0.9 ? 2 : 4;
-              return grid;
-            }
-          }
-        }
-      }
-    },
-    isGridFull(grid){
-      for(let i in grid){
-        for(let j in grid[i]){
-          if(grid[i][j]===0) return false;
-        }
-      }
-      return true;
-    },
-    moveGrid(grid, direction) {
-      const newGrid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-
-      let mergeCount = 0;
-      for (let i = 0; i < 4; i++) {
-        const isRow = (direction === 'left' || direction === 'right');
-        const isReverse = (direction === 'down' || direction === 'right');
-
-        // 根据方向提取一行或一列
-        let line = [];
-        if (isRow) {
-          line = grid[i];
-        } else {
-          for (let j = 0; j < 4; j++) line.push(grid[j][i]);
-        }
-        line = line.filter(val => val !== 0);
-
-        if(isReverse) line.reverse();
-
-        // 处理合并
-        let newLine = [];
-        while (line.length) {
-          if (line[1] && line[0] === line[1]) {
-            line.shift();
-            line[0] *= 2;
-            mergeCount += line[0];
-          }
-          newLine.push(line.shift());
-        }
-
-        // 填充空白
-        while (newLine.length < 4) newLine.push(0);
-
-        if (isReverse) newLine.reverse();
-
-        // 更新到新格子
-        for (let j = 0; j < 4; j++) {
-          if (isRow) {
-            newGrid[i][j] = newLine[j];
-          } else {
-            newGrid[j][i] = newLine[j];
-          }
-        }
-      }
-
-      return {
-        grid: newGrid,
-        mergeCount: mergeCount
-      };
-    },
-    saveProgress(){
-      storage.set({
-        key: "grid",
-        value: JSON.stringify(this.grid)
-      });
-      storage.set({
-        key: "score",
-        value: JSON.stringify(this.score)
-      });
-      storage.set({
-        key: "highScore",
-        value: JSON.stringify(this.highScore)
-      });
-    }
   },
   onInit() {
     storage.get({
       key: "grid",
-      default: JSON.stringify(this.newGrid()),
-      success: str => { this.grid = JSON.parse(str) }
+      default: "",
+      success: str => {
+        if(str==="") return this.initGrid();
+        this.grid = JSON.parse(str);
+      }
     });
     storage.get({
       key: "score",
@@ -153,6 +52,12 @@ export default {
       default: "0",
       success: str => { this.highScore = JSON.parse(str) }
     });
+  },
+  onDestroy() {
+    this.saveProgress();
+  },
+  err(v){
+    throw new Error(v);
   },
   onLeftSideSwipe(e) {
     if (e.direction === "right"){
@@ -169,7 +74,6 @@ export default {
   onTopSideSwipe(e){
     if (e.direction === "down"){
       if(this.showTopSideSwipe){
-        // if (this.sideSwipeTimeout) clearTimeout(this.sideSwipeTimeout);
         router.replace({uri: "/pages/about/about"});
       } else{
         this.showTopSideSwipe = true;
@@ -180,20 +84,139 @@ export default {
     }
   },
   onGridSwipe(e){
-    let { grid, mergeCount } = this.moveGrid(this.grid,e.direction);
-    if(JSON.stringify(grid)!==JSON.stringify(this.grid)){
-      grid = this.addCellNumber(grid);
-      this.grid = grid;
+    const oldGrid = this.grid.join(",");
+    let mergeCount = this.moveGrid(e.direction);
+    if (oldGrid !== this.grid.join(",")){
+      this.addCellNumber();
       this.score += mergeCount;
+      this.refreshGrid();
+      this.saveProgress();
+      return;
     }
-    this.saveProgress();
   },
   onReplaySwipe(e){
     if (e.direction === "right") {
       this.highScore = Math.max(this.highScore, this.score);
       this.score = 0;
-      this.grid = this.newGrid();
+      this.initGrid();
+      this.refreshGrid();
       this.saveProgress();
     }
   },
+  initGrid(){
+    this.grid = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    this.addCellNumber();
+    this.addCellNumber();
+  },
+  addCellNumber(){
+    let zeroCells = [];
+    for (let i = 0; i < 16; i++) if(this.grid[i] === 0) zeroCells.push(i);
+    let idx = Math.floor(Math.random() * zeroCells.length);
+    this.grid[zeroCells[idx]] = Math.random() < 0.9 ? 2 : 4;
+  },
+  moveGrid(direction) {
+    let mergeCount = 0;
+    const isRow = (direction === 'left' || direction === 'right');
+    const isReverse = (direction === 'down' || direction === 'right');
+    for (let i = 0; i < 4; i++) {
+      // 根据方向提取一行或一列
+      let line;
+      if (isRow) {
+        if (isReverse) {
+          line = [this.grid[i*4+3],this.grid[i*4+2],this.grid[i*4+1],this.grid[i*4]];
+        } else {
+          line = [this.grid[i*4],this.grid[i*4+1],this.grid[i*4+2],this.grid[i*4+3]];
+        }
+      } else {
+        if (isReverse) {
+          line = [this.grid[i+12],this.grid[i+8],this.grid[i+4],this.grid[i]];
+        } else{
+          line = [this.grid[i],this.grid[i+4],this.grid[i+8],this.grid[i+12]];
+        }
+      }
+      // line = line.filter(cell => cell !== 0);
+
+      // 处理合并
+      let write = 0;
+      for (let read = 0; read < 4; ) {
+        if (line[read] === 0) {
+          read++;
+          continue;
+        }
+
+        let cell = line[read];
+
+        let next = read + 1;
+        while (next < 4 && line[next] === 0) {
+          next++;
+        }
+
+        if (next < 4 && line[read] === line[next]) {
+          cell *= 2;
+          mergeCount += cell;
+          read = next + 1;
+        } else {
+          read++;
+        }
+
+        line[write++] = cell;
+      }
+
+      // 填充空白
+      while (write < 4) {
+        line[write++] = 0;
+      }
+
+      // // 处理合并
+      // let newLine = [];
+      // for (let i = 0; i < line.length; i++) {
+      //   let cell = line[i];
+      //   if (i < line.length - 1 && cell === line[i + 1]) {
+      //     cell *= 2;
+      //     mergeCount += cell;
+      //     i++;
+      //   }
+      //   newLine.push(cell);
+      // }
+      //
+      // // 填充空白
+      // while (newLine.length < 4) newLine.push(0);
+
+      // 更新到新格子
+      for (let j = 0; j < 4; j++) {
+        if (isRow) {
+          if (isReverse){
+            this.grid[i * 4 + j] = line[3 - j];
+          } else{
+            this.grid[i * 4 + j] = line[j];
+          }
+        } else {
+          if (isReverse) {
+            this.grid[j * 4 + i] = line[3 - j];
+          } else{
+            this.grid[j * 4 + i] = line[j];
+          }
+        }
+      }
+    }
+
+    return mergeCount;
+  },
+  refreshGrid(){
+    this.grid = this.grid;
+  },
+  saveProgress(){
+    storage.set({
+      key: "grid",
+      value: JSON.stringify(this.grid)
+    });
+    storage.set({
+      key: "score",
+      value: JSON.stringify(this.score)
+    });
+    storage.set({
+      key: "highScore",
+      value: JSON.stringify(this.highScore)
+    });
+  }
 }
